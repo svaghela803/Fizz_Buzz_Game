df92df290484675a286196abec897c1ac6334343
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-27 14:46:29 +0100

    added if else statement

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,4 +1,21 @@
 public class FizzBuzz {
 
+    public static String game(int number){
+        if(number % 3 == 0 && number % 5 == 0){
+            return "fizz buzz";
+        } else if (number % 5 == 0) {
+            return "buzz";
+        } else if (number % 3 == 0){
+            return "fizz";
+        }
+        return String.valueOf(number);
+
+
+
+
+
+
+
+    }
     
 }
